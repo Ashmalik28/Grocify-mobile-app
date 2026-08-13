89d888e586f204cf7a08f7e24977d64aba125dba
Ashmalik28 <ashyammalik052@gmail.com>
2026-08-13 16:39:59 +0530

    created native tab bar with liquid glass effect

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect , Stack } from "expo-router";
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
+
+export default function Layout(){
+    const {isSignedIn , isLoaded} = useAuth();
+
+    if(!isLoaded){
+        return null;
+    }
+
+    if(!isSignedIn){
+        return <Redirect href="/(auth)/sign-in" />
+    }
+
+    return  (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>List</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf={{
+            default : "list.bullet.clipboard" ,
+            selected : "list.bullet.clipboard.fill"
+        }} md="list" />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="planner">
+        <NativeTabs.Trigger.Icon sf={{
+            default : "plus.circle" ,
+            selected : "plus.circle.fill"
+        }} md="add" />
+        <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="insights">
+        <NativeTabs.Trigger.Icon sf={{
+            default : "chart.bar" ,
+            selected : "chart.bar.fill" 
+        }} md="analytics" />
+        <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+
+    );
+}
---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const InsightsScreen = () => {
+  return (
+    <View>
+      <Text>Insight Screen</Text>
+    </View>
+  )
+}
+
+export default InsightsScreen;
