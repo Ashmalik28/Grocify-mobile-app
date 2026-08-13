db675b0a2b68a8d51be94c93116694808bb09516
Ashmalik28 <ashyammalik052@gmail.com>
2026-08-13 16:22:38 +0530

    added neon database and pushed dummy data

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,10 +1,16 @@
 
-import { View , Text } from 'react-native'
+import { useClerk } from '@clerk/expo';
+import { PreventRemoveContext } from 'expo-router/build/react-navigation';
+import { View , Text, Pressable } from 'react-native'
 
 export default function Home(){
+    const {signOut} = useClerk();
     return (
         <View>
         <Text className='text-white'> Hello fron the Home page </Text>
+        <Pressable onPress={() => signOut()}>
+        <Text className='text-white'>Sign out</Text>
+        </Pressable>
         </View>
     );
 }
