5244d3c9226fe01ec5fc40fc9f9fea67a28c14b9
Ashmalik28 <ashyammalik052@gmail.com>
2026-08-16 18:51:09 +0530

    Built the planner screen page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,9 +3,10 @@ import "../../global.css"
 import { ClerkProvider } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
 import { useColorScheme } from "react-native";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 if (!publishableKey) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
@@ -14,10 +15,12 @@ if (!publishableKey) {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache} >
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{headerShown : false}} />
-      </ThemeProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <KeyboardProvider>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
+      </KeyboardProvider>
     </ClerkProvider>
   ) 
 }
